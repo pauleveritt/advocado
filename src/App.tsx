fff3915bca66c51c2c19d8e54847cf8af84c7cb3
Paul Everitt <pauleveritt@me.com>
2018-01-31 09:54:59 -0500

    Basic start on routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
+
+import { Container, Box, Notification } from 'bloomer';
+
 import Employees from './Employees';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h1>Welcome to Advocado</h1>
-                <p>It's bigly.</p>
-                <h2>Employees</h2>
-                <Employees/>
-            </div>
+            <Container isFluid={true} style={{marginTop: 10}}>
+                <Notification isColor="warning">This container is <strong>centered</strong> on desktop.</Notification>
+
+                <Box>Hello World!
+
+                    <div>
+                        <h1>Welcome to Advocado</h1>
+                        <p>It's bigly.</p>
+                        <h2>Employees</h2>
+                        <Employees/>
+                    </div></Box>
+            </Container>
         );
     }
 }
